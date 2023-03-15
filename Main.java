e7eb609ec9ee79e7f38d50aa4eec4bf81d54b490
Song Yang <songyang.dev@gmail.com>
2023-03-15 15:57:26 -0400

    Rename files and add assertions

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
   public static void main(String[] args) {
     CleanedSolution solution = new CleanedSolution();
-    System.out.println(solution.isValid("()"));
-    System.out.println(solution.isValid("[]"));
-    System.out.println(solution.isValid("[][]"));
-    System.out.println(solution.isValid("["));
-    System.out.println(solution.isValid("[()]"));
-    System.out.println(solution.isValid("[()]{}"));
-    System.out.println(solution.isValid("[()]{]"));
+    assert solution.isValid("()") == true;
+    assert solution.isValid("[]") == true;
+    assert solution.isValid("[][]") == true;
+    assert solution.isValid("[") == false;
+    assert solution.isValid("[()]") == true;
+    assert solution.isValid("[()]{}") == true;
+    assert solution.isValid("[()]{]") == false;
   }
 
 }
